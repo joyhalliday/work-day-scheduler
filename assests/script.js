014ecebc6c6c6c075c0658b1cb1b5809b29acfc6
Joy Halliday <tjoyhalliday@gmail.com>
2022-09-23 20:43:57 -0700

    addeded date blocks

--- assests/script.js
+++ assests/script.js
@@ -1,3 +1,27 @@
 var today = moment();
 $(currentDay).text(today.format("[Today is] dddd MMM Do, YYYY"));
 
+var currentHour = new Date().getHours();
+
+console.log(currentHour);
+
+$("textarea").each(function() {
+console.log($(this).attr("id"))
+var textHour = parseInt($(this).attr("id").split("-")[1])
+if(textHour<currentHour) {
+    $(this).addClass("past")
+} else if (textHour === currentHour) {
+    $(this).addClass("present")
+} else {
+    $(this).addClass("future")
+}
+
+} )
+
+$.parseJSON$(`text-${buttonId}`).text
+
+
+//add event listeners to buttons on click
+//when click use (this) to extract id
+//$(`text-${buttonId}`).text
+//save text to local storage
