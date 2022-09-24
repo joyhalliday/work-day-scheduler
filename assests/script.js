fd780ad18a0ebc96270c758ef7d33cc93c5b8620
Joy Halliday <tjoyhalliday@gmail.com>
2022-09-23 21:49:18 -0700

    final commit

--- assests/script.js
+++ assests/script.js
@@ -1,13 +1,13 @@
-var today = moment();
+const today = moment();
 $(currentDay).text(today.format("[Today is] dddd MMM Do, YYYY"));
 
-var currentHour = new Date().getHours();
+const currentHour = new Date().getHours();
 
 console.log(currentHour);
 
 $("textarea").each(function() {
 console.log($(this).attr("id"))
-var textHour = parseInt($(this).attr("id").split("-")[1])
+const textHour = parseInt($(this).attr("id").split("-")[1])
 if(textHour<currentHour) {
     $(this).addClass("past")
 } else if (textHour === currentHour) {
@@ -15,15 +15,12 @@ if(textHour<currentHour) {
 } else {
     $(this).addClass("future")
 }
+} );
 
-} )
-
-var buttonId = $(".saveBtn");
+const buttonId = $(".saveBtn");
 
 buttonId.on('click', function () {
    console.log("Save");
    localStorage.setItem(`text-${buttonId}`).text
 });
-
-
 
